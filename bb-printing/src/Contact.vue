b0700b3b0523e2113d6af1a2dd909156d195a111
WantanAsyraff <147963245+WantanAsyraff@users.noreply.github.com>
2025-09-04 20:01:24 +0800

    contact
    almost donao

--- bb-printing/src/Contact.vue
+++ bb-printing/src/Contact.vue
@@ -1,18 +1,27 @@
 <script setup>
     import infoFooter from './components/Footer.vue'
+    const phone = '60108865575';
+    const msg = encodeURIComponent('Hello, I would like to inquire about your T-shirts.');
+    const whatsappLink = `https://api.whatsapp.com/send?phone=${phone}&text=${msg}`;
 </script>
 
 <template>
     <!--Main content-->
     <h1>Get In Touch!</h1>
     <div class="contact-wrapper">
+        <!--Whatsapp-->
         <div class="contact-card">
             <div class="whatsapp">
                 <h3>whatsapp</h3>
-
+                 <a :href="whatsappLink" target="_blank">Contact Us on WhatsApp!</a>
             </div>
-            <div class="email">
-                <h3>email</h3>
+        </div>
+        <!--Email-->
+        <div class="contact-card">
+            <div class="facebook">
+                <h3>FACEBOOK</h3>
+                <a href="https://www.facebook.com/bbwearprinting/photos?locale=ms_MY">Check out our facebook</a>
+
             </div>
         </div>
     </div>
@@ -26,6 +35,40 @@
     }
 
     .contact-wrapper {
+        text-align: center;
         display: flex;
+        gap: 2rem;
+    }
+
+    .contact-card {
+        margin: 4rem;
+        padding: 8rem;
+        border-radius: 3rem;
+        background-color: white;
+        transition: 1s cubic-bezier(0.165, 0.84, 0.44, 1);
+    }
+
+    .contact-card:hover {
+        transform: scale(1.05);
+    }
+
+    @media (max-width:480px) {
+        h3 {
+            font-size: 20px;
+            padding-bottom: 1rem;
+        }
+
+        .contact-wrapper {
+            text-align: center;
+            display: flex;
+            gap: 0.3rem;
+    }
+
+    .contact-card {
+        margin: 1rem;
+        padding: 1rem;
+        border-radius: 1rem;
+        background-color: white;
+    }
     }
 </style>
